6e1f0f236277b9e5bfda856db7e61a4529ee94a8
Rafay Yousafzai <rafaykhan.mesh@gmail.com>
2024-08-21 19:30:19 +0500

    projects

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,27 +43,17 @@ export default function RootLayout({
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-          <div className="relative flex flex-col h-screen">
+          <div className="flex flex-col h-screen">
             <Navbar />
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
               {children}
             </main>
-            <FloatingDock
-              items={siteConfig?.navItems}
-              mobileClassName="translate-y-20" // only for demo, remove for production
-            />
-            <br />
-            {/* <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://nextui-docs-v2.vercel.app?utm_source=next-app-template"
-                title="nextui.org homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">NextUI</p>
-              </Link>
-            </footer> */}
+            <footer className="w-full flex items-center justify-center py-3 fixed bottom-0 bg-transparent">
+              <FloatingDock
+                items={siteConfig?.navItems}
+                mobileClassName="translate-y-20"
+              />
+            </footer>
           </div>
         </Providers>
       </body>
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,13 @@
+import { ExpandableCard } from "@/components/ExpandableCard";
 import { title } from "@/components/primitives";
 
 export default function ProjectsPage() {
   return (
     <div>
       <h1 className={title()}>Projects</h1>
+      <section className="w-full my-8">
+        <ExpandableCard />
+      </section>
     </div>
   );
 }
