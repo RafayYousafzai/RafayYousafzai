919a702b3c96b73778f33ea68820df1a72a3c28f
Rafay Yousafzai <rafaykhan.mesh@gmail.com>
2024-08-23 20:41:19 +0500

    map list and verify

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,5 @@
 // next-sitemap.config.js
 module.exports = {
-    siteUrl: 'https://rafaykhan.vercel.app/',
-    generateRobotsTxt: true, // (Optional) Generate robots.txt file
-  };
-  
+  siteUrl: "https://rafaykhan.vercel.app/",
+  generateRobotsTxt: true, // (Optional) Generate robots.txt file
+};
